97219e87e12996fc80535391ef01b6359cef86eb
Конишевский Антон Игоревич <anton.konishevskij@privatbank.ua>
2019-04-19 14:39:38 +0300

    url fix

--- admin/language/en-gb/extension/payment/liqpay_checkout.php
+++ admin/language/en-gb/extension/payment/liqpay_checkout.php
@@ -13,7 +13,7 @@ $_['text_liqpay']		 = '<img src="view/image/payment/liqpay.png" alt="LIQPAY" tit
 // Entry
 $_['entry_public_key']	 = 'Public Key';
 $_['entry_private_key']	 = 'Private Key';
-$_['entry_api']	         = 'https://www.liqpay.com/api/3/checkout';
+$_['entry_api']	         = 'https://www.liqpay.ua/api/3/checkout';
 $_['entry_action']	     = 'pay';
 $_['entry_type']		 = 'Type';
 $_['entry_total']		 = 'Total';
