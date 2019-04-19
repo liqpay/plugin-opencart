fcfdab3c900c4a132c43268e2480435e6e10b9f3
Конишевский Антон Игоревич <anton.konishevskij@privatbank.ua>
2019-04-19 14:57:22 +0300

    url fix

--- admin/controller/payment/liqpay.php
+++ admin/controller/payment/liqpay.php
@@ -18,14 +18,14 @@
  * EXTENSION INFORMATION
  *
  * OpenCart         1.5.6
- * LiqPay API       https://www.liqpay.com/ru/doc
+ * LiqPay API       https://www.liqpay.ua/documentation/ru
  *
  */
 
 /**
  * Payment method liqpay controller (admin)
  *
- * @author      Liqpay <support@liqpay.com>
+ * @author      Liqpay <support@liqpay.ua>
  */
 class ControllerPaymentLiqPay extends Controller
 {
@@ -141,7 +141,7 @@ class ControllerPaymentLiqPay extends Controller
 		} else {
 			$this->data['liqpay_action'] = $this->config->get('liqpay_action');
 			if (empty($this->data['liqpay_action'])) {
-				$this->data['liqpay_action'] = 'https://www.liqpay.com/api/checkout';
+				$this->data['liqpay_action'] = 'https://www.liqpay.ua/api/checkout';
 			}
 		}
 
